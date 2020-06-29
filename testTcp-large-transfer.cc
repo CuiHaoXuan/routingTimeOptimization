c29e07d357978c99f89c26606e90245bcaf12050
Mohammed Gharib <gharib_2020@yahoo.com>
2020-06-29 11:45:43 -0700

    initializeTheproject

---
+++ testTcp-large-transfer.cc
@@ -0,0 +1,316 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+//
+// Network topology
+//
+//           10Mb/s, 10ms       10Mb/s, 10ms
+//       n0-----------------n1-----------------n2
+//
+//
+// - Tracing of queues and packet receptions to file 
+//   "tcp-large-transfer.tr"
+// - pcap traces also generated in the following files
+//   "tcp-large-transfer-$n-$i.pcap" where n and i represent node and interface
+// numbers respectively
+//  Usage (e.g.): ./waf --run tcp-large-transfer
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "ns3/core-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/netanim-module.h"
+#include "ns3/aodv-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/wifi-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("testTcpLargeTransfer");
+
+// The number of bytes to send in this simulation.
+static const uint32_t totalTxBytes = 2000000;
+static uint32_t currentTxBytes = 0;
+// Perform series of 1040 byte writes (this is a multiple of 26 since
+// we want to detect data splicing in the output stream)
+static const uint32_t writeSize = 1040;
+uint8_t data[writeSize];
+
+// These are for starting the writing process, and handling the sending 
+// socket's notification upcalls (events).  These two together more or less
+// implement a sending "Application", although not a proper ns3::Application
+// subclass.
+
+void StartFlow (Ptr<Socket>, Ipv4Address, uint16_t);
+void WriteUntilBufferFull (Ptr<Socket>, uint32_t);
+
+static void 
+CwndTracer (uint32_t oldval, uint32_t newval)
+{
+  NS_LOG_INFO ("Moving cwnd from " << oldval << " to " << newval);
+}
+
+int main (int argc, char *argv[])
+{
+  // Users may find it convenient to turn on explicit debugging
+  // for selected modules; the below lines suggest how to do this
+  //  LogComponentEnable("TcpL4Protocol", LOG_LEVEL_ALL);
+  //  LogComponentEnable("TcpSocketImpl", LOG_LEVEL_ALL);
+  //  LogComponentEnable("PacketSink", LOG_LEVEL_ALL);
+  //  LogComponentEnable("TcpLargeTransfer", LOG_LEVEL_ALL);
+
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+
+  // initialize the tx buffer.
+  for(uint32_t i = 0; i < writeSize; ++i)
+    {
+      char m = toascii (97 + i % 26);
+      data[i] = m;
+    }
+
+  // Here, we will explicitly create three nodes.  The first container contains
+  // nodes 0 and 1 from the diagram above, and the second one contains nodes
+  // 1 and 2.  This reflects the channel connectivity, and will be used to
+  // install the network interfaces and connect them with a channel.
+  NodeContainer adhocNodes;
+  adhocNodes.Create (3);
+
+/*
+  NodeContainer n1n2;
+  n1n2.Add (n0n1.Get (1));
+  n1n2.Create (1);
+*/
+
+  // We create the channels first without any IP addressing information
+  // First make and configure the helper, so that it will put the appropriate
+  // attributes on the network interfaces and channels we are about to install.
+  WifiHelper wifi;
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
+
+  YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
+  YansWifiChannelHelper wifiChannel;
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
+  wifiPhy.SetChannel (wifiChannel.Create ());
+
+// Add a mac and disable rate control
+  WifiMacHelper wifiMac;
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                "DataMode",StringValue ("DsssRate11Mbps"),
+                                "ControlMode",StringValue ("DsssRate11Mbps"));
+  wifiMac.SetType ("ns3::AdhocWifiMac");
+
+  double txp = 7.5;
+  wifiPhy.Set ("TxPowerStart",DoubleValue (txp));
+  wifiPhy.Set ("TxPowerEnd", DoubleValue (txp));
+
+  NetDeviceContainer adhocDevices = wifi.Install (wifiPhy, wifiMac, adhocNodes);
+
+  //wifi.SetDeviceAttribute ("DataRate", DataRateValue (DataRate (10000000)));
+  //wifi.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (10)));
+
+  // And then install devices and channels connecting our topology.
+  //NetDeviceContainer dev0 = p2p.Install (n0n1);
+  //NetDeviceContainer dev1 = p2p.Install (n1n2);
+
+
+//setup mobility
+  MobilityHelper mobilityAdhoc;
+
+int64_t streamIndex = 0; // used to get consistent mobility across scenarios
+
+  ObjectFactory pos;
+  pos.SetTypeId ("ns3::RandomRectanglePositionAllocator");
+  pos.Set ("X", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=100.0]"));
+  pos.Set ("Y", StringValue ("ns3::UniformRandomVariable[Min=0.0|Max=300.0]"));
+
+  Ptr<PositionAllocator> taPositionAlloc = pos.Create ()->GetObject<PositionAllocator> ();
+  streamIndex += taPositionAlloc->AssignStreams (streamIndex);
+
+  int nodeSpeed=20;  //in m/s
+  int nodePause = 0; //in s
+  std::stringstream ssSpeed;
+  ssSpeed << "ns3::UniformRandomVariable[Min=0.0|Max=" << nodeSpeed << "]";
+  std::stringstream ssPause;
+  ssPause << "ns3::ConstantRandomVariable[Constant=" << nodePause << "]";
+
+  mobilityAdhoc.SetMobilityModel ("ns3::RandomWaypointMobilityModel",
+                                  "Speed", StringValue (ssSpeed.str ()),
+                                  "Pause", StringValue (ssPause.str ()),
+                                  "PositionAllocator", PointerValue (taPositionAlloc));
+
+
+/*If you want to fix the node positions
+
+mobilityAdhoc.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+        Vector node1_Position(0.1, 0.1, 0.0);
+	Vector node2_Position(50.0, 0.1,0.0);
+	Vector node3_Position(100.0, 0.1, 0.0);	
+
+	ListPositionAllocator myListPositionAllocator;
+	myListPositionAllocator.Add(node1_Position);
+	myListPositionAllocator.Add(node2_Position);
+	myListPositionAllocator.Add(node3_Position);
+	
+	mobilityAdhoc.SetPositionAllocator(&myListPositionAllocator);
+*/
+
+  mobilityAdhoc.SetPositionAllocator (taPositionAlloc);
+  mobilityAdhoc.Install (adhocNodes);
+  streamIndex += mobilityAdhoc.AssignStreams (adhocNodes, streamIndex);
+  NS_UNUSED (streamIndex); // From this point, streamIndex is unused
+//end of setup mobility
+
+ 
+AodvHelper aodv;
+  Ipv4ListRoutingHelper list;
+  list.Add (aodv, 100);
+
+
+  // Now add ip/tcp stack to all nodes.
+  
+  InternetStackHelper internet;
+  internet.SetRoutingHelper (list);
+  internet.InstallAll ();
+
+
+
+  //internet.InstallAll ();
+
+  // Later, we add IP addresses.
+  Ipv4AddressHelper addressAdhoc;
+  addressAdhoc.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer adhocInterfaces;
+  adhocInterfaces = addressAdhoc.Assign (adhocDevices);
+  //ipv4.Assign (dev0);
+  //ipv4.SetBase ("10.1.2.0", "255.255.255.0");
+  //Ipv4InterfaceContainer ipInterfs = ipv4.Assign (dev1);
+
+  // and setup ip routing tables to get total ip-level connectivity.
+  //Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Simulation 1
+  //
+  // Send 2000000 bytes over a connection to server port 50000 at time 0
+  // Should observe SYN exchange, a lot of data segments and ACKS, and FIN 
+  // exchange.  FIN exchange isn't quite compliant with TCP spec (see release
+  // notes for more info)
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  uint16_t Port = 50000;
+
+/*onOff
+OnOffHelper onoff1 ("ns3::TcpSocketFactory",Address ());
+  onoff1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"));//it is the default value [Constant=1.0]
+  onoff1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0.0]"));//to keep it sending cbr traffic at constant rate
+*/
+
+  // Create a packet sink to receive these packets on n2...
+  PacketSinkHelper sink ("ns3::TcpSocketFactory",
+                         InetSocketAddress (Ipv4Address::GetAny (), Port));
+
+  ApplicationContainer apps = sink.Install (adhocNodes.Get (0));
+  apps.Start (Seconds (0.0));
+  apps.Stop (Seconds (3000.0));
+
+  // Create a source to send packets from n0.  Instead of a full Application
+  // and the helper APIs you might see in other example files, this example
+  // will use sockets directly and register some socket callbacks as a sending
+  // "Application".
+
+  // Create and bind the socket...
+  Ptr<Socket> localSocket =
+    Socket::CreateSocket (adhocNodes.Get (2), TcpSocketFactory::GetTypeId ());
+  localSocket->Bind ();
+
+  // Trace changes to the congestion window
+  Config::ConnectWithoutContext ("/NodeList/0/$ns3::TcpL4Protocol/SocketList/0/CongestionWindow", MakeCallback (&CwndTracer));
+
+  // ...and schedule the sending "Application"; This is similar to what an 
+  // ns3::Application subclass would do internally.
+  Simulator::ScheduleNow (&StartFlow, localSocket,
+                          adhocInterfaces.GetAddress (0), Port);
+
+  // One can toggle the comment for the following line on or off to see the
+  // effects of finite send buffer modelling.  One can also change the size of
+  // said buffer.
+
+  //localSocket->SetAttribute("SndBufSize", UintegerValue(4096));
+
+  //Ask for ASCII and pcap traces of network traffic
+  AsciiTraceHelper ascii;
+  wifiPhy.EnableAsciiAll (ascii.CreateFileStream ("traceFiles/test/test-TCPlaregTransfer/test-tcp-large-transfer.tr"));
+  wifiPhy.EnablePcapAll ("traceFiles/test/test-TCPlaregTransfer/tcp-large-transfer");
+
+AnimationInterface anim ("traceFiles/test/test-TCPlaregTransfer/tcp-large-transfer.xml");
+
+  // Finally, set up the simulator to run.  The 1000 second hard limit is a
+  // failsafe in case some change above causes the simulation to never end
+  Simulator::Stop (Seconds (3000));
+  Simulator::Run ();
+  Simulator::Destroy ();
+}
+
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//begin implementation of sending "Application"
+void StartFlow (Ptr<Socket> localSocket,
+                Ipv4Address servAddress,
+                uint16_t Port)
+{
+  NS_LOG_LOGIC ("Starting flow at time " <<  Simulator::Now ().GetSeconds ());
+  localSocket->Connect (InetSocketAddress (servAddress, Port)); //connect
+
+  // tell the tcp implementation to call WriteUntilBufferFull again
+  // if we blocked and new tx buffer space becomes available
+  localSocket->SetSendCallback (MakeCallback (&WriteUntilBufferFull));
+  WriteUntilBufferFull (localSocket, localSocket->GetTxAvailable ());
+}
+
+void WriteUntilBufferFull (Ptr<Socket> localSocket, uint32_t txSpace)
+{
+  while (currentTxBytes < totalTxBytes && localSocket->GetTxAvailable () > 0) 
+    {
+      uint32_t left = totalTxBytes - currentTxBytes;
+      uint32_t dataOffset = currentTxBytes % writeSize;
+      uint32_t toWrite = writeSize - dataOffset;
+      toWrite = std::min (toWrite, left);
+      toWrite = std::min (toWrite, localSocket->GetTxAvailable ());
+      int amountSent = localSocket->Send (&data[dataOffset], toWrite, 0);
+      if(amountSent < 0)
+        {
+          // we will be called again when new tx space becomes available.
+          return;  
+        }
+      currentTxBytes += amountSent;
+    }
+  localSocket->Close ();
+}
